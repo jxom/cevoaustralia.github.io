24b269243d449e3a14b80df5e6ea475e0fb623a5
Elliott Gorrell <elliott.gorrell@cevo.com.au>
2017-09-15 10:17:08 +1000

    enable scroll to anchors on all pages

--- cevo-hugo/static/js/cevo.js
+++ cevo-hugo/static/js/cevo.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 
   // Smooth scrolling via animate()
   $("a").on('click', function(event) {
-    if (this.hash && window.location.pathname == "/") {
+    if (this.hash) {
       event.preventDefault();
       var hash = this.hash;
       $('html, body').animate({
